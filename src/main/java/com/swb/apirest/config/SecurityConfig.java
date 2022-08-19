c90b125274dc0e473d9130a24334a4202952a404
Alexandre Freire <103282161+alexandrefreire1@users.noreply.github.com>
2022-08-18 21:37:09 -0300

    Update SecurityConfig.java

--- src/main/java/com/swb/apirest/config/SecurityConfig.java
+++ src/main/java/com/swb/apirest/config/SecurityConfig.java
@@ -23,6 +23,7 @@ public class SecurityConfig {
                 .and().csrf()
                 .disable()
                 .authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/api/usuario").permitAll()
                 .and().formLogin().permitAll()
                 .and().authorizeRequests().anyRequest()
                 .authenticated();
